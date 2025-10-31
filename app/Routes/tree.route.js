97c9f5d94c76db689ee078dfb0e146a3cb7580b2
student <armand.briffaux@oclock.school>
2025-10-31 14:41:37 +0100

    ajout du middleware de validation pour Tree

--- app/Routes/tree.route.js
+++ app/Routes/tree.route.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import  TreeController from '../Controllers/tree-controller.js';
+import { validateTree } from '../Middlewares/tree.middleware.js';
 
 const router = express.Router();
 
 // Routes CRUD
 router.get('/', TreeController.getAll);
 router.get('/:id', TreeController.getById);
-router.post('/', TreeController.create);
-router.put('/:id', TreeController.update);
+router.post('/',validateTree, TreeController.create);
+router.put('/:id',validateTree, TreeController.update);
 router.delete('/:id', TreeController.delete);
 
 export default router;
