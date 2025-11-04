61b99738eae483a76d9b8d2d85458df1ab892bba
student <armand.briffaux@oclock.school>
2025-11-04 08:43:50 +0100

    modif de seed-tables-js pour crée un utilisateur admin

--- app/Routes/tree.route.js
+++ app/Routes/tree.route.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import TreeController from '../Controllers/tree-controller.js';
+import { authenticateToken } from '../Middlewares/auth-middleware.js';
+import { isAdmin } from '../Middlewares/isAdmin-middleware.js';
 
 const router = express.Router();
 
 // CRUD basique
 router.get('/', TreeController.getAll);
-router.get('/create', TreeController.create);
-router.post('/create', TreeController.create);
+router.get('/create', authenticateToken, isAdmin, TreeController.create);
+router.post('/create', authenticateToken, isAdmin, TreeController.create);
 router.get('/:id', TreeController.getById);
-router.get('/:id/edit', TreeController.update);
-router.post('/:id/edit', TreeController.update);
+router.get('/:id/edit', authenticateToken, isAdmin, TreeController.update);
+router.post('/:id/edit', authenticateToken, isAdmin, TreeController.update);
 
 // Suppression via DELETE (JS fetch)
-router.delete('/:id', TreeController.delete);
+router.delete('/:id', authenticateToken, isAdmin, TreeController.delete);
 
 export default router;
