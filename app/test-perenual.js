879f193e1c4554807e1743cc4e250e7eb072b056
student <armand.briffaux@oclock.school>
2025-11-07 13:15:10 +0100

    ajout de l'api pour les nom scientifiques

--- app/test-perenual.js
+++ app/test-perenual.js
@@ -1,17 +1,23 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "../.env" });
+
+
+console.log("Ma clé Perenual :", process.env.PERENUAL_TOKEN);
 
 const token = process.env.PERENUAL_TOKEN;
 
 const testPerenual = async () => {
-    try {
-        const response = await fetch(`https://perenual.com/api/species-list?key=${token}&q=oak`);
-        const data = await response.json();
-        console.log(data);
-    } catch (err) {
-        console.error("Erreur :", err);
-    }
+  try {
+    const url = `https://perenual.com/api/species-list?key=${token}&q=oak`;
+    console.log("URL utilisée :", url);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error("Erreur :", err);
+  }
 };
 
 testPerenual();
+
 
