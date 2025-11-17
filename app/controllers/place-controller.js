b53efa9cfa75e6f002679169b93b9ea488204b8b
ZakariaDahhane <zakaria.dahhane@oclock.school>
2025-11-17 12:00:31 +0100

    fix: l'ajout d'un lieu

--- app/controllers/place-controller.js
+++ app/controllers/place-controller.js
@@ -5,6 +5,17 @@ import CoreController from './core-controller.js';
 // Schéma Joi pour valider les données d'un Lieux
 const placeSchema = Joi.object({
   name: Joi.string().min(2).required(), // Le nom doit faire au moins 2 caractères et est obligatoire
+  description: Joi.string().allow(''), // Description / optionnelle
+    image: Joi.string().allow('').optional().custom((value, helpers) => { // url ou chemin relatif, optionnelle
+      //si vide ok
+      if (!value || value === '') return value;
+  
+      // si rempli, doit être une URL OU un chemin relatif (/images/...)
+      if (value.startsWith('http://') || value.startsWith('https://') || value.startsWith('/images/')) {
+        return value;
+      }
+      return helpers.message('L\'image doit être une URL ou un chemin vers /images/');
+    }),
 });
 
 // Contrôleur spécifique aux lieux, hérite du CoreController
